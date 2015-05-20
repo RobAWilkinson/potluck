aefb22d603be0d13a4503c39cd4d6abd5e8d0949
Ladan Nasserian <ladannasserian@gmail.com>
2015-05-20 15:15:01 -0700

    created view dir, defined host and use middleware, began defining routes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express'),
   morgan = require('morgan'),
   // body parser gives us post request inside of our request.body.<attribute>
   // gives us POST params in request.body
-  bodyparser = require('body-parser')
+  bodyParser = require('body-parser')
   // simulate PUT & DELETE
   methodOverride = require('method-override');
 
@@ -20,6 +20,7 @@ var express = require('express'),
 mongoose.connect('mongodb://localhost:27017/potluck');
 
 // create a model, remember a model is a representation of our database
+// this sets up our schema
 var Blog = mongoose.model( 'Blog', {
     title: String,
     content: String,
@@ -33,7 +34,26 @@ var Blog = mongoose.model( 'Blog', {
 // cool things for us
 // two underscores before dirname
 // we are telling it to look for the directoy views
+// set is configuring app
 app.set('views', path.join(__dirname, 'views'));
 // we want to set it to use jade, a more syntactic HTML that is defaulted with
-// express
+// express, its compressed HTML
 app.set('view engine', 'jade');
+
+// use is using the middleware
+// we are calling our middleware, using it in our app, we want to hide this
+// in production so noone sees what requests we are asking for
+app.use(morgan('dev'));
+// helps make our post request more readable
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// lests us override post request and simulate puts and delete request
+// we are using this method later on in our forms to override post request
+app.use(methodOverride('_method'));
+
+// Establish the routes for our app
+app.get('/', function(request, response))
+
+// defining our host
+app.listen(3000);
+console.log('App is listening on port 3000');
